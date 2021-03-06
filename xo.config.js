244c8d91a881294091a6044af88b6735740f340f
Cosmin Popovici <hellocosmin@gmail.com>
2021-03-06 16:40:24 +0200

    style: fix linting error

--- xo.config.js
+++ xo.config.js
@@ -5,6 +5,7 @@ module.exports = {
     complexity: 0,
     'capitalized-comments': 0,
     'unicorn/string-content': 0,
+    'unicorn/no-array-for-each': 0,
     'promise/prefer-await-to-then': 0,
     'unicorn/no-abusive-eslint-disable': 0,
     quotes: ['error', 'single', {allowTemplateLiterals: true}]
